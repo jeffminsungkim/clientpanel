3a11cf797da2e6c52ef5b801a084d8f544fe5489
JeffMinsungKim <ziznzkak@gmail.com>
2017-11-08 15:49:51 +0900

    Add client component has created

--- src/app/components/add-client/add-client.component.ts
+++ src/app/components/add-client/add-client.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Client } from '../../models/Client';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { ClientService } from '../../services/client.service';
 
 @Component({
   selector: 'app-add-client',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddClientComponent implements OnInit {
 
-  constructor() { }
+  c: Client = {
+    firstName: '';
+    lastName: '';
+    email: '';
+    phone: '',
+    balance: 0
+  }
+
+  client: Object = {
+    value: {},
+    valid: false
+  }
+
+  disableBalanceOnAdd: boolean = true;
+
+  constructor(private flashMessagesService: FlashMessagesService, private router: Router, private clientService: ClientService) { }
 
   ngOnInit() {
   }
 
+  onSubmit(client) {
+    if (this.disableBalanceOnAdd) {
+      client.value.balance = 0;
+    }
+    if (!client.valid) {
+      this.flashMessagesService.show('Please fill in all fields', {cssClass:'alert-danger', timeout: 4000});
+      this.router.navigate(['add-client']);
+    } else {
+      this.clientService.newClient(client.value);
+      this.flashMessagesService.show('New client added', {cssClass:'alert-success', timeout: 4000});
+      this.router.navigate(['/']);
+    }
+  }
 }
