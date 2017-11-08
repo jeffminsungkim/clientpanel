d5f077c96906da3b07632ff1ff305e70fbc1c10b
JeffMinsungKim <ziznzkak@gmail.com>
2017-11-09 00:39:35 +0900

    Register component updated

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService} from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router, private flashMessagesService: FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.authService.register(this.email, this.password).then((response) => {
+      this.flashMessagesService.show('New user registered!', {cssClass:'alert-success', timeout: 2000});
+      this.router.navigate(['/']);
+    }).catch((err) => {
+      this.flashMessagesService.show(err.message, {cssClass:'alert-danger', timeout: 4000});
+      this.router.navigate(['/register']);
+    });
   }
 
 }
