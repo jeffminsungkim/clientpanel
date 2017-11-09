57c20b69f1f77ca4c8f465c74687d3f928a317d3
JeffMinsungKim <ziznzkak@gmail.com>
2017-11-09 21:02:13 +0900

    Service that deals with settings object

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -4,14 +4,26 @@ import { Settings } from '../models/settings';
 @Injectable()
 export class SettingsService {
   settings: Settings = {
-    allowRegistration: false,
+    allowRegistration: true,
     disableBalanceOnAdd: true,
     disableBalanceOnEdit: true
   }
 
-  constructor() { }
+  constructor() {
+    // Used VanillaJS localStorage method in order to store info in browser. Nothing to do with Angular.
+    // Details: https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+    if (localStorage.getItem('settings') != null) {
+      // When we're getting it, parsing it to an object from a string
+      this.settings = JSON.parse(localStorage.getItem('settings'));
+    }
+  }
 
   getSettings() {
     return this.settings;
+  }
+
+  updateSettings(settings: Settings){
+    // When we insert it to the localStorage, we're taking an object and turning it into a string before we put it in.
+    localStorage.setItem('settings', JSON.stringify(settings));
   }
 }
