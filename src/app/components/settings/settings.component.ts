bf2cb93fe818511e8f072a377bf07bb88d83284f
JeffMinsungKim <ziznzkak@gmail.com>
2017-11-09 20:59:10 +0900

    Settings component updated

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { ClientService } from '../../services/client.service';
+import { SettingsService } from '../../services/settings.service';
+import { Settings } from '../../models/Settings';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  settings: Settings;
 
-  constructor() { }
+  constructor(
+    public settingsService: SettingsService,
+    public flashMessagesService: FlashMessagesService,
+    public router: Router) { }
 
   ngOnInit() {
+    this.settings = this.settingsService.getSettings();
   }
 
+  onSubmit() {
+    this.settingsService.updateSettings(this.settings);
+    this.flashMessagesService.show('Sucessfully updated!', {cssClass:'alert-success', timeout: 3000});
+    this.router.navigate(['/settings']);
+  }
 }
